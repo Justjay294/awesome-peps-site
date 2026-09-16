42f07e6c15259efc0de3e4b33a905eeab7acfee3
Justjay294 <jghvac@hotmail.com>
2026-09-16 14:28:31 -0400

    Create Service-worker.js

---
+++ Service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "awesome-peps-v1";
+
+const APP_FILES = [
+  "/",
+  "/index.html",
+  "/products.js",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
